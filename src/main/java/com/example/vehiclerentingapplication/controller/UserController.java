33e1a04ac0293e3b00142e724d33a08cd370830a
Aryan Sahu <aryan17sahu@gmail.com>
2024-12-11 16:20:15 +0530

    Registration Done

---
+++ src/main/java/com/example/vehiclerentingapplication/controller/UserController.java
@@ -0,0 +1,32 @@
+package com.example.vehiclerentingapplication.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.vehiclerentingapplication.entity.User;
+import com.example.vehiclerentingapplication.service.UserService;
+import com.example.vehiclerentingapplication.util.ResponseStructure;
+
+@RestController
+public class UserController {
+	
+	private final UserService userService;
+
+	public UserController(UserService userService) {
+		super();
+		this.userService = userService;
+	}
+	
+	@PostMapping("/register")
+	public ResponseEntity<ResponseStructure<User>>registration(@RequestBody User user){
+		user = userService.register(user);
+		
+		return ResponseEntity.status(HttpStatus.CREATED).body(ResponseStructure.create(HttpStatus.CREATED.value(), "customer created", user));
+	}
+	
+	
+
+}
